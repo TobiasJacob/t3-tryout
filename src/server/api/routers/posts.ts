55c8eff0ecfcbe95116fa8d031ac5da4d5c1e016
Tobias Jacob <tobi.jacob@gmx.net>
2023-05-01 20:10:21 +0200

    Added rate limiting

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,7 +3,22 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
+import dayjs from "dayjs";
 
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
 
 export const postsRouter = createTRPCRouter({
   // Returns a list of all osts with author information
@@ -50,6 +65,15 @@ export const postsRouter = createTRPCRouter({
     content: z.string().min(1).max(255).nonempty(),
   })).mutation(async ({ ctx, input }) => {
     const authorId = ctx.currentUserId;
+
+    const { success, reset } = await ratelimit.limit(authorId);
+
+    if (!success) {
+      throw new TRPCError({
+        code: "TOO_MANY_REQUESTS",
+        message: `You have exceeded the rate limit for this action. Try again after ${dayjs(reset).format()}.`,
+      });
+    }
 
     await ctx.prisma.post.create({
       data: {
