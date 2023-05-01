784f019c829bfeeb8d8160dfc56593d885d17b1f
Tobias Jacob <tobi.jacob@gmx.net>
2023-05-01 14:58:45 +0200

    Added posts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,10 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,49 @@
+import clerkClient from "@clerk/clerk-sdk-node";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  // Returns a list of all osts with author information
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const users = await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    });
+
+    const userDict = users.reduce<Record<string, { id: string, userName?: string, eMail?: string, profileImageUrl: string }>>((acc, user) => {
+      acc[user.id] = {
+        id: user.id,
+        userName: user.username ?? undefined,
+        eMail: user.emailAddresses[0]?.emailAddress ?? undefined,
+        profileImageUrl: user.profileImageUrl,
+      };
+      return acc;
+    }, {});
+
+    const postsWithAuthor = posts.map((post) => {
+      const author = userDict[post.authorId];
+
+      if (!author) throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: `Author not found for post ${post.id}`,
+      });
+
+      return {
+        post,
+        author,
+      };
+    });
+
+    return postsWithAuthor;
   }),
 });
